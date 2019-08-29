6057d7af5de6dd6b67457bf8a77742b910a3183c
github username <github email address>
2019-08-29 10:41:59 -0700

    deleted test code info

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,42 +14,6 @@ describe "Users Controller" do
 
     # let's take a look at available RSPEC & Capybara 'macros'
 
-
-        it "can visit '/login'" do
-            get '/login'
-            expect(last_response.status).to eq(200)
-        end
-
-        it "'/login' has a input field for name" do
-            visit '/login'
-            expect(page).to have_field('name')
-        end
-
-        it "'/login' has a input field for password" do
-            visit '/login'
-            expect(page).to have_field('password')
-        end
-
-        it "'/login' allows a user to login on form submission" do
-            visit '/login'
-            fill_in('name', :with => 'Jane')
-            fill_in('password', :with => 'idk what this is going to do')
-            find_button('submit').click
-        end
-
-        it "'/login' has input field" do
-            visit '/login'
-            within('label#name') do
-                expect(page).to have_content("Your Name:") 
-            end
-        end
-
-        it "'/login' will route to '/signup' if selected" do
-            visit '/login'
-            # fill_in('name', :with => 'Jane')
-            # fill_in('password', :with => 'idk what this is going to do')
-            click_link('Sign Up!')
-            expect(page.current_path).to eq('/signup')
-        end
+    
 
 end
